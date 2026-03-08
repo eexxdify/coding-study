50d63771032b369d20eb979b620ca3aaac1206cc
eexxdify <eexxdify@gamil.com>
2026-03-08 13:56:57 +0000

    변수를 이용한 기초문제

---
+++ 01_basic/variables.py
@@ -0,0 +1,14 @@
+
+# 변수를 활용한 합, 곱
+number1 = 6
+number2 = 4
+print(number1+number2)
+print(number1*number2)
+
+# 이름을 입력받아 인사
+name = "eexxdify"
+print("안녕하세요", name +"님")
+
+# 정수를 입력받아 2배 출력
+number = 20
+print(number*2)
