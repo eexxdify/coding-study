ed5562d1f8b094aa27c38b0068de2a2e59b2bc69
eexxdify <eexxdify@gmail.com>
2026-03-14 11:46:07 +0000

    함수를 이용한 문제

--- 01_basic/function.py
+++ 01_basic/function.py
@@ -1,36 +1,36 @@
-# Quiz) 당신은 Cocoa 서비스를 이용하는 택시 기사입니다.
-# 50명의 승객과 매칭 기회가 있을 때, 총 탑승 승객 수를 구하는 프로그램을 작성하시오.
+# Quiz) 표준 체중을 구하는 프로그램을 작성하시오.
 
-# 조건 1 : 승객별 운행 소요 시간은 5분 ~ 50분 사이의 난수로 정해집니다.
-# 조건 2 : 당신은 소요 시간 5분 ~ 15분 사이의 승객만 매칭해야 합니다.
+# * 표준 체중 : 각 개인의 키에 적당한 체중
 
-# (출력문 예제)
-# [o] 1번째 손님 (소요시간 : 15분)
-# [ ] 2번째 손님 (소요시간 : 50분)
-# [o] 3번째 손님 (소요시간 : 5분)
-# ...
-# [ ] 50번째 손님 (소요시간 : 16분)
+# (성별에 따른 공식)
+# 남자 : 키(m) x 키(m) x 22
+# 여자 : 키(m) x 키(m) x 21 
 
-# 총 탑승 승객 : 2분
+# 조건 1 : 표준 체중은 별도의 함수 내에서 계산
+#           * 함수명 : std_weight
+#           * 전달값 : 키(height), 성별(gender)
+# 조건 2 : 표준 체중은 소수점 둘째자리까지 표시
 
-# 내 답안
-from random import *
-for customer in range(1, 51): # 승객은 인원은 있으나, 총 탑승 승객 수가 없음
-  time_taken = randrange(5, 51)
-  if time_kaken in randrange(5, 51): # randrange()는 난수를 다시 생성하는 함수, 조건 비교에 맞지않음. 
-    print("[o] {0}번째 손님 (소요시간 : {1}분)".format(customer, time_taken))
-  else:
-    print("[ ] {0}번째 손님 (소요시간 : {1}분)".format(customer, time_taken))
+# (출력 예제)
+# 키 175cm 남자의 표준 체중은 67.38kg 입니다.
 
+
+# 내 답
+def std_weight(height, gender):
+  if gender == "남자":
+    return height * height * 22
+    print("키 {0}cm {1}의 표준 체중은 {0}kg 입니다.".format(height, gender))
+  elif gender == "여자":
+    return height  * height * 21
+# 함수까진 어느정도 만들었으나 그 이후 출력문을 구사하지 못함.
 
 # 답안
-from random import *
-final_customer = 0 # 총 탑승 승객 수
-for customer in range(1, 51): # 1~50 (승객)
-  time_taken = randrange(5, 51) # 소요시간 5분 ~ 50분
-  if 5 <= time_taken <= 15: # 소요시간 5분 ~ 15분
-    print("[o] {0}번째 손님 (소요시간 : {1}분)".format(customer, time_taken))
-    final_customer += 1
-  else: # 매칭실패
-    print("[ ] {0}번째 손님 (소요시간 : {1}분)".format(customer, time_taken))
-print("총 탑승 승객 : {}".format(final_customer))
+def std_weight(height, gender): # 키 m 단위 (실수), 성별 "남자" / "여자"
+  if gender == "남자":
+    return height * height * 22
+  else:
+    return height * height * 21
+height = 175 # cm 단위
+gender = "남자"
+weight = round(std_weight(height / 100, gender), 2) # height / 100을 나눠서 1.75m로 만듬
+print("키 {0}cm {1}의 표준 체중은 {2}kg 입니다.".format(height, gender, weight))
