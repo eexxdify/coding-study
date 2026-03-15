0c366a3afd0509b6d9eaf713a65579fa04d02ee4
eexxdify <eexxdify@gmail.com>
2026-03-16 00:40:17 +0900

    파일입출력을 이용한 문제 (수정)
     GitHub업로드시 줄바꿈 코드가 작동해서 업로드, GitHub에서 직접수정

--- 01_basic/File_input_output.py
+++ 01_basic/File_input_output.py
@@ -26,9 +26,6 @@ while True:
 for i in range(1, 51):
   with open(str(i) + "주차.txt", "w", encoding="utf8") as report_file:
     report_file.write("- {0} 주차 주간보고 -".format(i))
-    report_file.write("
-부서 :")
-    report_file.write("
-이름 :")
-    report_file.write("
-업무 요약 :")
+    report_file.write("\n부서 :")
+    report_file.write("\n이름 :")
+    report_file.write("\n업무 요약 :")
